493c31ee4f70cb75786c02f5aed0f356c7d2c9be
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2021-12-17 13:09:06 +0530

    working on vedoes

--- static/app.js
+++ static/app.js
@@ -1,5 +1,38 @@
 
 
+// Get Subscribers
+const youtubeKey = 'AIzaSyDAob2jCLtCUWivr2KIPO1LPVAvIz1q-gs';
+const youtubeUser = 'UCvSrWeilliH9cDUrn-CR3oQ';
+const delay = 1000; // 10 min
+
+let getSubscribers = () => {
+
+    fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+        subCount.innerHTML = data["items"][0].statistics.subscriberCount;
+    })
+
+}
+
+setInterval(() => {
+    getSubscribers();
+}, delay);
+
+
+
+var channelID = "UCvSrWeilliH9cDUrn-CR3oQ";
+var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
+   var link = data.items[0].link;
+   var id = link.substr(link.indexOf("=")+1);
+$("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+});
+
+
 $(window).load(function () {
 
  
@@ -14,14 +47,14 @@ $(window).load(function () {
 //     // $(this).css("color" , "red");
 // });
 var a = (Math.random()*1)+d ;
-var r = (Math.random()*-2+1);
+var r = (Math.floor(Math.random()*-2+1));
     // Run code
     setInterval(() => {
  
         $('.rotate').each(function(){
           
             a+=0.01;
-            if(r>0){
+            if(r==0){
                  $(this).css('transform',  'rotate('+ -a + 'deg)');
             }
             else{
@@ -33,5 +66,5 @@ var r = (Math.random()*-2+1);
 
 $('.rotate').hover(function(){
     $(this).addClass("pluse" , "animated");
-    console.log("yo");
+    console.log((Math.floor(Math.random()*-2+1)));
   });
